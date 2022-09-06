2b8ea667b2a668524c7130aa86d8b3d40d6514e9
Andrea Jared <adj0803@gmail.com>
2022-09-06 14:21:03 -0400

    package.json

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
 const questions =() =>
   inquirer.prompt([
     {
